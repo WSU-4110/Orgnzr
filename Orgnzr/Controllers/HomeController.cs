847a79ecf3ee6c6cf0fd40ebb151eeb7ad071a34
Sam Church <church.richard.s@gmail.com>
2020-10-05 17:52:43 -0400

    Start of Navigation Bar, Modified Homecontroller.cs, Added Search.cshtml, Modified _Layout.cshtml

--- Orgnzr/Controllers/HomeController.cs
+++ Orgnzr/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace Orgnzr.Controllers
             return View();
         }
 
+        public IActionResult Search()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
