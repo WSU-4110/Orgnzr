01d744af259d046d3a76fdf4e2ab218467c2a2a8
Ken OHanlon <fj7615@wayne.edu>
2020-10-05 20:26:25 -0400

    Creation of clientContact.cs class under Models folder to manage client contacts. Creation of contactContext.cs under project folder to assign database context and to create a rudimentary entity set. Installments of EntityFrameworkCore to support entity creation and utilization.

---
+++ Orgnzr/Models/clientContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orgnzr.Models
+{
+    public enum preferredContact
+    {
+        Text, Phone, Email
+    }
+    public class clientContact
+    {
+        public string firstName { get; set}
+        public string lastName { get; set}
+        public string phoneNumber { get; set}
+        public string emailAddress { get; set }
+        public preferredContact? preferredContact { get; set}
+    }
+}
---
+++ Orgnzr/Project/Data/contactContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Orgnzr.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Orgnzr.Data
+{
+    public class contactContext : DbContext
+    {
+        public contactContext(DbContextOptions<contactContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<clientContact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<clientContact>().ToTable("Contact");
+        }
+    }
+}
