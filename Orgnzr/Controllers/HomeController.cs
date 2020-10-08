8d796dee980bfb3ed0efcb21ac7e7c76c508b556
Sam Church <church.richard.s@gmail.com>
2020-10-07 21:47:18 -0400

    FR1. Navigation Bar V1: Sam Church: October 07, 2020

--- Orgnzr/Controllers/HomeController.cs
+++ Orgnzr/Controllers/HomeController.cs
@@ -35,6 +35,16 @@ namespace Orgnzr.Controllers
             return View();
         }
 
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+
+        public IActionResult Inventory()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
