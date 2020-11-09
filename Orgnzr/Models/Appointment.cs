17e8b93cddadd15d1d44e85b13cc5c1571a15d5d
Sam Church <church.richard.s@gmail.com>
2020-11-09 13:35:51 -0500

    FR8/9: Add/Remove Appoint ments - Implemetning the Singleton Design Pattern, essentially creating a single entity of type Appointmnet that has multiple entities stored into a Database

--- Orgnzr/Models/Appointment.cs
+++ Orgnzr/Models/Appointment.cs
@@ -6,31 +6,31 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Orgnzr.Models
-{
-
-    public class Appointment
-    {
-        [Key]
-        public int appointmentID { get; set; }
-        public int appointmentDay { get; set; }
-        public int appoitnmentMonth { get; set; }
-        public int appointmentYear { get; set; }
-        public int appointmentStartHour { get; set; }
-        public int appointmentStartMinute { get; set; }
-        public int appointmentFinishHour { get; set; }
-        public int appointmentFinishMinute { get; set; }
-        public double appointmentDuration { get; set; }
-
-        [ForeignKey("clientId")]
-        public virtual ClientContact Client { get; set; }
-        [Display(Name = "Client ID")]
-        public virtual int? clientId {get; set;}
-
-        [ForeignKey("serviceId")]
-        public virtual Services Service { get; set; }
-        [Display(Name = "Service ID")]
-        public virtual int? serviceId { get; set; }
-
-    }
-}
+namespace Orgnzr.Models 
+{ 
+     
+    public class Appointment 
+    { 
+        [Key] 
+        public int appointmentID { get; set; } 
+        public int appointmentDay { get; set; } 
+        public int appoitnmentMonth { get; set; } 
+        public int appointmentYear { get; set; } 
+        public int appointmentStartHour { get; set; } 
+        public int appointmentStartMinute { get; set; } 
+        public int appointmentFinishHour { get; set; } 
+        public int appointmentFinishMinute { get; set; } 
+        public double appointmentDuration { get; set; } 
+         
+        [ForeignKey("clientId")] 
+        public virtual ClientContact Client { get; set; } 
+        [Display(Name = "Client ID")] 
+        public virtual int? clientId {get; set;} 
+         
+        [ForeignKey("serviceId")] 
+        public virtual Services Service { get; set; } 
+        [Display(Name = "Service ID")] 
+        public virtual int? serviceId { get; set; } 
+         
+    } 
+} 
