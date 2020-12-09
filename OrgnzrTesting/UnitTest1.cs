eedf350c2b9f5446589351df1f4703efa5a30dfc
Kenneth O'Hanlon <fj7615@wayne.edu>
2020-12-08 21:11:45 -0500

    testing commits

---
+++ OrgnzrTesting/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace OrgnzrTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
+C: \Users\kenne\Documents\WSU - csUndergrad\Fall2020\CSC4111\Orgnzr\OrgnzrTests\OrgnzrTesting\OrgnzrTesting\OrgnzrTesting.csproj
---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Orgnzr
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
